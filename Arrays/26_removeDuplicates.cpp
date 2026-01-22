23abe8aa7a5601cac71d8428bfddde52612bc277
Afwaan Ansari <afwaanansari@gmail.com>
2026-01-22 21:37:39 +0530

    17 jan 2026

---
+++ Arrays/26_removeDuplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0, j = 1, k = 1;
+        while (j < nums.size()) {
+            if (nums[j] == nums[j - 1])
+                j++;
+            else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+                k++;
+            }
+        }
+        return k;
+    }
+};
